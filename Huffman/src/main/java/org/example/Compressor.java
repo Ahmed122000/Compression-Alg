7942577b8b5a7f901dd25da6db5412be5d3daf74
Ahmed Hesham <ahmedhesham122000@gmail.com>
2024-05-10 03:20:59 +0300

    finish compressor

---
+++ Huffman/src/main/java/org/example/Compressor.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class FileHandler {
+
+    Encoder encoder;
+    public FileHandler(Encoder encoder){
+        this.encoder = encoder;
+    }
+    void WriteCompressedFile(String outputFilePath){
+
+    }
+}
